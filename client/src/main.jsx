b000a6e88be164239178b1da4353c9bbc50004ab
mrsidyot77 <mrsidyot@gmail.com>
2024-09-06 23:26:23 +0530

    typo

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import  {Store} from "./redux/Store.js"
-import  {persistor} from "./redux/Store.js"
+import { Store, persistor } from './redux/Store.js';
 import {Provider} from "react-redux"
 import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <Provider store={Store}>
     <PersistGate persistor={persistor} loading = {null}>
     <App />
     </PersistGate>
