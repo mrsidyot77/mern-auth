b20d215c0a1436148688daf7dc55be34d2c1af55
mrsidyot77 <mrsidyot@gmail.com>
2024-09-08 18:00:02 +0530

    changes on weekend to resolve dot env config error

--- api/src/index.js
+++ api/src/index.js
@@ -4,7 +4,8 @@ import app from "./app.js";
 import dotenv from "dotenv"
 import path from "path"
 
-dotenv.config({ path: "./.env" });
+dotenv.config({ path: path.resolve(process.cwd(), ".env") });
+
 
 conectDB()
 .then(()=>{
