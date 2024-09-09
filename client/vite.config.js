a56a70c4031a7de1c2e25e6b4fae86566e161b39
mrsidyot77 <mrsidyot@gmail.com>
2024-09-09 13:36:49 +0530

    remove rollUp options

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: 'src/main.jsx'
-      }
-    },
     emptyOutDir: false  // Ensure Vite does not delete files in the `dist` directory
   }
 });
