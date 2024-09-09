0db8699a439cffef3f25ca1333da626dfc59f626
mrsidyot77 <mrsidyot@gmail.com>
2024-09-09 15:59:47 +0530

    cahnge in a env var import

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -35,7 +35,7 @@ function SignIn() {
     e.preventDefault()
     try {
       dispatch(signInStart())
-      const res = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/users/login`,{
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/users/login`,{
         method: "POST",
         headers: {
           "Content-Type" : "application/json"
