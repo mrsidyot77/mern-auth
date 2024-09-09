fc38876485d5fa1ace94ea78443e83b30422904d
mrsidyot77 <mrsidyot@gmail.com>
2024-09-09 11:46:57 +0530

    debbugin in api/app.js

--- api/src/app.js
+++ api/src/app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import errorHandler from "./middlewares/errorHandler.middleware.js";
 import path from "path";
 import { fileURLToPath } from "url";
+import fs from 'fs';  // Import fs module for file operations
 
 // Construct __filename and __dirname in ES module
 const __filename = fileURLToPath(import.meta.url);
@@ -13,13 +14,24 @@ const app = express();
 // Serve static files from the client/dist directory
 app.use(express.static(path.join(__dirname, "../../client/dist")));
 
-// All other routes should serve the index.html file
+// Route to list files in client/dist directory for debugging
+app.get('/list-files', (req, res) => {
+    const directoryPath = path.join(__dirname, '../../client/dist');
+    fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+            return res.status(500).send('Unable to scan directory: ' + err);
+        }
+        res.send(files);
+    });
+});
+
+// Catch-all route to serve the index.html file
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../../client/dist", "index.html"));
 });
 
 // Log the resolved path for debugging
-console.log("Resolved path:", path.join(__dirname, "../../client/dist", "index.html"));;
+console.log("Resolved path:", path.join(__dirname, "../../client/dist", "index.html"));
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
