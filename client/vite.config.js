f1b27b15a27cb7796b03ec59a3180615f71c2c03
mrsidyot77 <mrsidyot@gmail.com>
2024-09-09 13:00:01 +0530

    vitConfige edited

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // Output directory for the build
     rollupOptions: {
       input: {
-        main: 'src/main.jsx'
-      }
-    }
-  }
+        main: 'src/main.jsx', // Entry point of your application
+      },
+    },
+  },
 });
