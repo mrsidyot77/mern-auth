069369b05f6cbd0c4490581d1ace363dd0737a6e
mrsidyot77 <mrsidyot@gmail.com>
2024-09-09 11:18:57 +0530

    typo fixes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {Store,persistor} from "./redux/Store.js"
+import {Store,persistor} from "./redux/Store"
 import {Provider} from "react-redux"
 import { PersistGate } from 'redux-persist/integration/react'
 
